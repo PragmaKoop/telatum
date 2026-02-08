3988f9e2aa9ddf024b2f2da1554eed296c67fc6d
Kerem <kerem@pragmakoop.com>
2026-02-08 18:59:25 +0300

    Added top navigation links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,36 +17,20 @@ export const sharedPageComponents: SharedLayout = {
   ],
   afterBody: [],
   footer: Component.Footer({
-  links: {
-    // Leave this empty to have zero external links
-  },
-}),
+    links: {},
+  }),
 }
 
-// components for pages that display a single page (e.g. a single note)
+// components for pages that display a single slot (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
+    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
     Component.Explorer(),
   ],
   right: [
@@ -56,22 +40,4 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
-export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
-  right: [],
-}
+// components for pages that display lists of pages  (e.g
